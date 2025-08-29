f9618fe7e8981b6d81e8ad975cd5dfa9206708f8
Gitesh Dewangan <121309558+GiteshDewangan@users.noreply.github.com>
2025-08-29 07:37:48 +0530

    Create Implementation of stack using array.cpp

---
+++ Implementation of stack using array.cpp
@@ -0,0 +1,55 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class Stack{
+    
+public:
+    int arr[5];
+    int idx;
+    
+    Stack(){
+        idx = -1;
+    }
+    void push(int val ){
+        if(idx == sizeof(arr) / sizeof(arr[0])  -1 ){
+            cout<< "stack is full";
+            return;
+        }
+        idx++;
+        arr[idx] = val;
+        
+    }
+    void pop(){
+        if(idx==-1){
+            cout<<"Stack is empty";
+            return;
+        }
+        idx--;
+    }
+    int top(){
+        
+        if(idx==-1){
+            cout<<"Stack is empty";
+            return -1;
+        }
+        return arr[idx];
+    }
+    int size(){
+        return idx+1;
+    }
+};
+
+int main() {
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout<<st.size()<<" ";
+    st.pop();
+    cout<<st.size()<<" ";
+    
+    
+    
+    return 0;
+} 
